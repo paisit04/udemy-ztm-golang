aeb55b9bc247bb379a7e679f22242bc902ecdfdb
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-05-19 10:06:08 -0700

    Section 8: Final Milestone Project: Pixl

---
+++ src/projects/pixl/ui/types.go
@@ -0,0 +1,16 @@
+package ui
+
+import (
+	"pixl/apptype"
+	"pixl/pxcanvas"
+	"pixl/swatch"
+
+	"fyne.io/fyne/v2"
+)
+
+type AppInit struct {
+	PixlCanvas *pxcanvas.PxCanvas
+	PixlWindow fyne.Window
+	State      *apptype.State
+	Swatches   []*swatch.Swatch
+}
